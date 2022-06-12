571295a4e4e36edbcd6493334f3be4351428590f
kuld1854 <101596294+kuld1854@users.noreply.github.com>
2022-06-12 10:48:28 +0530

    Create 1-12.cpp

---
+++ 1-12.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     long long int n;
+     cin>>n;
+     int count= 0;
+     while(n){
+         if(n%10 == 4 || n%10 == 7)
+             count++;
+          n/=10;
+     }
+     if( count != 0 && (count == 4 || count  == 7))
+          cout<<"YES"<<endl;
+        else
+          cout<<"NO"<<endl;
+   
+}
